38d2889e31c5356f7f268790608e7144ab4ff1c8
Breno de mendonça aguiar <67332235+BrenoAguiar308@users.noreply.github.com>
2021-03-31 14:53:26 -0300

    Códico arrumado, ajustado e com funções adicionadas.
    Códico com erros corrigidos, funções esperadas e tudo comentado. :)

--- AC1.ino
+++ AC1.ino
@@ -1,62 +1,105 @@
 //variaveis da led
 const int vermelho = 5;
 const int verde = 6;
-const int azul = 7
-
-bool estadoLedVermelho = false;
-
+const int azul = 7;
+//variaveis do botão
 const int botao1 = 2;
+const int botao2 = 3;
+
+
+
+//variaveis de armazenamento de número 
+
+
+
+
 unsigned long lastDebounceTime1 = 0;
+unsigned long lastDebounceTime2 = 0;
+
+//Tempo de delay para poder clicar o botão de novo
 const int botaoDelay = 100;
 
 void setup()
+  // deifinir as portas do arduino como entrada ou saida
 {
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   
   pinMode(vermelho, OUTPUT);
   pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
+  pinMode(azul, OUTPUT);
   
+  pinMode(botao1, INPUT);
+  	pinMode(botao2, INPUT);
+  //configura a taxa de dados para 9600 bps
   Serial.begin(9600);
-	
+	//dados para mostrar no monitor serial
   Serial.println("AC1 - Meu Primeiro Projeto 2021");
   Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
+  Serial.println("Grupo: DripGroup              ");
 }
 
 void loop()
 {
-  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("botao 1 apertado");
+
+// configuração dos botões de quando eles delvem ligar e
+  // de que forma e as suas informações para o monitor serial
+   if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
+      Serial.println("botao 1 apertado");
+     digitalWrite(vermelho, HIGH);
     ledVermelho();
-  	lastDebounceTime1 = millis();
-  }
+      lastDebounceTime1 = millis();
+   }
+
+   
   
-  if(getTemperatura() > 30){
-    ledAzul(true);
+   if((millis() - lastDebounceTime2) > botaoDelay && digitalRead(botao2)){
+      Serial.println("botao 2 apertado");
+     digitalWrite(vermelho,LOW);
+     ledVermelho();
+      lastDebounceTime2 = millis();
+     
+    // Defini que a led verde ligue se a luminosidade estiver maior que 5
+     
+   }
+  if(getLuminosidade() > 5){
+    ledVerde(true);
+    Serial.println("luminosidade acima do ideal");
   }else{
-  	ledAzul(falso); 
+    ledVerde(false);
+    Serial.println("luminosidade ok");
+    
+    //defini que a led ligue se a temperatura passar de 15.
+}
+  if(getTemperatura() > 15){
+    ledAzul(true);
+    Serial.println("temperatura acima do ideal");
+  }else{
+ledAzul(false);
+    Serial.println("temperatura ok");
+    
+                  
   }
-  	
   delay(10);
 }
 
+//funções que não devem retornar nenhuma informação para sua função chamada
 void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
+  
 }
-void ledVerde(){
- //precisa fazer  
+void ledVerde(bool estado){
+  digitalWrite(verde,estado);
 }
 void ledAzul(bool estado){
 	digitalWrite(azul,estado);
+    
 }
-
+//Configurações do sensor de temperatura e luminosidade
 int getTemperatura(){
   	int temperaturaC;
 	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
   	return temperaturaC;
+    
 } 
 
 int getLuminosidade(){
